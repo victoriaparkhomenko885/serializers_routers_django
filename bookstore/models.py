86f8f01beb18daff2dc5edfb95efd8a89ff3de07
Victoria <55848341+victoriaparkhomenko885@users.noreply.github.com>
2020-07-05 19:05:44 +0300

    add models

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=48)
+    address = models.CharField(max_length=256)
+    city = models.CharField(max_length=128)
+    site = models.URLField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=48)
+    last_name = models.CharField(max_length=48)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=512)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.title} ({self.publication_date})'
