af580c402f9e2bc4e636951bf637b8c9ee68d5fb
Victoria <55848341+victoriaparkhomenko885@users.noreply.github.com>
2020-07-05 19:55:53 +0300

    add DRF router

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,9 +1,9 @@
+from django.urls import path, include
 from rest_framework import routers
-
-from . import views
 
 router = routers.DefaultRouter()
 
-
-router.register('book', views.BookViewSet)
-router.register('publisher', views.PublisherViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
